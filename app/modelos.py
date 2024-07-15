8a1dd76521300b20ddeecf05648eacfe62cb1fe2
Natalia Tsyleva <tsylevanatalia@gmail.com>
2024-07-15 21:15:42 +0200

    Creado VistaEntrada para usar como Inputs de datos en nuestra interfaz

--- app/modelos.py
+++ app/modelos.py
@@ -1,44 +1,48 @@
+
+
 from enum import Enum, auto
+from collections import namedtuple
+
+Datos_Entrada = namedtuple("Datos_Entrada", ("precio", "edad_max"))
+
 
 class TipoEntrada(Enum):
-    BEBE = 0
-    NIÑO = 14
-    ADULTO = 23
-    JUBILADO = 18
-    
+    BABY = Datos_Entrada(0, 2)
+    KID = Datos_Entrada(15, 12)
+    ADULT = Datos_Entrada(25, 64)
+    YAYO = Datos_Entrada(20, 99)
 
 class Entrada:
     def __init__(self, edad: int):
-        self.__validate_edad(edad)
-        self.__edad = edad
         
-        if edad <= 2:
-            self.tipo = TipoEntrada.BEBE
-            self.precio = 0
-        elif edad < 13:
-            self.tipo = TipoEntrada.NIÑO
-            self.precio = 14
-        elif edad < 65:
-            self.tipo = TipoEntrada.ADULTO
-            self.precio = 23
-        else:
-            self.tipo = TipoEntrada.JUBILADO
-            self.precio = 18
+        self.__validate_edad(edad)
+        
+        self.__edad = edad
+
+        for tipo in TipoEntrada:
+            if edad <= tipo.value.edad_max:
+                self.tipo = tipo
+                self.precio = tipo.value.precio
+                break
 
     def __validate_edad(self, edad):
         if edad < 0:
             raise ValueError("La edad no debe ser negativa")
+        elif edad > 99:
+            raise ValueError("La edad no puede ser mayor de 99")
 
 class Grupo_Entrada:
     def __init__(self):
         self.total = 0
         self.num_entradas = 0
-        self.tipos_entrada = {
-            TipoEntrada.BEBE: {"candidad": 0, "precio": 0},
-            TipoEntrada.NIÑO: {"candidad": 0, "precio": 14},
-            TipoEntrada.ADULTO: {"candidad": 0, "precio": 23},
-            TipoEntrada.JUBILADO: {"candidad": 0, "precio": 18}   
-        }
+
+        self.tipos_entrada = {}
+        for tipo in TipoEntrada:
+            self.tipos_entrada[tipo] = {'Q': 0, 'P': tipo.value.precio}
+        
+        # con dict comprehension
+        #self.tipos_entrada = {tipo: {'Q': 0, 'P': tipo.value} for tipo in TipoEntrada }
+
 
     def add_entrada(self, edad):
         """
@@ -49,10 +53,11 @@ class Grupo_Entrada:
         self.num_entradas += 1
         self.total += nueva_entrada.precio
 
-        self.tipos_entrada[nueva_entrada.tipo]["candidad"] += 1
+        self.tipos_entrada[nueva_entrada.tipo]["Q"] += 1
 
     def cantidad_entradas_por_tipo(self, tipo: TipoEntrada) -> int:
-        return self.tipos_entrada[tipo]["candidad"]
-    
+        return self.tipos_entrada[tipo]["Q"]
+
+
     def subtotal_tipo(self, tipo: TipoEntrada) -> int:
-        return self.tipos_entrada[tipo]["candidad"] * self.tipos_entrada[tipo]["precio"]
+        return self.tipos_entrada[tipo]["Q"] * self.tipos_entrada[tipo]["P"]
